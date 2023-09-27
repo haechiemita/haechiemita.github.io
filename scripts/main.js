868af98184baa1a49705ad7b5f24155a29074466
haechiemita <146155348+haechiemita@users.noreply.github.com>
2023-09-27 10:41:53 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+//Toggle & RESPONSIVE NAVIGATION//
+const navSlide = () => {
+const burger = document.querySelector(".burger")
+const navLists = document.querySelector("nav")
+
+burger.addEventListener("click", () => {
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+}
+  )
+}
+ navSlide();
+
+
+
+
+
+
+// clear form before unload//
+window.onbefoereunload = () => {
+  for (const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+};
